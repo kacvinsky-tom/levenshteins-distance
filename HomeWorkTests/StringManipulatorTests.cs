ece3b387d4c61f1fe1b1c9f7ec0e16364a84d809
Tomáš Kacvinský <485397@mail.muni.cz>
2024-06-29 20:08:55 +0200

    refactor: simplify variable names, typo

--- HomeWorkTests/StringManipulatorTests.cs
+++ HomeWorkTests/StringManipulatorTests.cs
@@ -11,9 +11,9 @@ public class StringManipulatorTests
     [TestMethod]
     [DataRow("testing", "testing")]
     [DataRow("this is the same string", "this is the same string")]
-    public void TrimPrefix_IdenticalStrings_ReturnsEmptyStrings(string first, string second)
+    public void TrimPrefix_IdenticalStrings_ReturnsEmptyStrings(string s1, string s2)
     {
-        var result = _stringManipulator.TrimPrefix(first, second);
+        var result = _stringManipulator.TrimPrefix(s1, s2);
 
         Assert.IsTrue(string.IsNullOrEmpty(result.Item1));
         Assert.IsTrue(string.IsNullOrEmpty(result.Item2));
@@ -22,9 +22,9 @@ public class StringManipulatorTests
     [TestMethod]
     [DataRow("testing", "testing")]
     [DataRow("this is the same string", "this is the same string")]
-    public void TrimSuffix_IdenticalStrings_ReturnsEmptyStrings(string first, string second)
+    public void TrimSuffix_IdenticalStrings_ReturnsEmptyStrings(string s1, string s2)
     {
-        var result = _stringManipulator.TrimSuffix(first, second);
+        var result = _stringManipulator.TrimSuffix(s1, s2);
 
         Assert.IsTrue(string.IsNullOrEmpty(result.Item1));
         Assert.IsTrue(string.IsNullOrEmpty(result.Item2));
@@ -33,9 +33,9 @@ public class StringManipulatorTests
     [TestMethod]
     [DataRow("hello world", "hello")]
     [DataRow("prefixString", "prefix")]
-    public void TrimPrefix_DifferentStrings_ReturnsSuffix(string first, string second)
+    public void TrimPrefix_DifferentStrings_ReturnsSuffix(string s1, string s2)
     {
-        var result = _stringManipulator.TrimPrefix(first, second);
+        var result = _stringManipulator.TrimPrefix(s1, s2);
 
         Assert.IsFalse(string.IsNullOrEmpty(result.Item1));
         Assert.IsTrue(string.IsNullOrEmpty(result.Item2));
@@ -44,9 +44,9 @@ public class StringManipulatorTests
     [TestMethod]
     [DataRow("world hello", "hello")]
     [DataRow("StringSuffix", "Suffix")]
-    public void TrimSuffix_DifferentStrings_ReturnsPrefix(string first, string second)
+    public void TrimSuffix_DifferentStrings_ReturnsPrefix(string s1, string s2)
     {
-        var result = _stringManipulator.TrimSuffix(first, second);
+        var result = _stringManipulator.TrimSuffix(s1, s2);
 
         Assert.IsFalse(string.IsNullOrEmpty(result.Item1));
         Assert.IsTrue(string.IsNullOrEmpty(result.Item2));
@@ -55,30 +55,30 @@ public class StringManipulatorTests
     [TestMethod]
     [DataRow("unrelated", "string")]
     [DataRow("different", "words")]
-    public void TrimPrefix_UnrelatedStrings_ReturnsOriginalStrings(string first, string second)
+    public void TrimPrefix_UnrelatedStrings_ReturnsOriginalStrings(string s1, string s2)
     {
-        var result = _stringManipulator.TrimPrefix(first, second);
+        var result = _stringManipulator.TrimPrefix(s1, s2);
 
-        Assert.AreEqual(first, result.Item1);
-        Assert.AreEqual(second, result.Item2);
+        Assert.AreEqual(s1, result.Item1);
+        Assert.AreEqual(s2, result.Item2);
     }
 
     [TestMethod]
     [DataRow("unrelated", "string")]
     [DataRow("different", "words")]
-    public void TrimSuffix_UnrelatedStrings_ReturnsOriginalStrings(string first, string second)
+    public void TrimSuffix_UnrelatedStrings_ReturnsOriginalStrings(string s1, string s2)
     {
-        var result = _stringManipulator.TrimSuffix(first, second);
+        var result = _stringManipulator.TrimSuffix(s1, s2);
 
-        Assert.AreEqual(first, result.Item1);
-        Assert.AreEqual(second, result.Item2);
+        Assert.AreEqual(s1, result.Item1);
+        Assert.AreEqual(s2, result.Item2);
     }
 
     [TestMethod]
     [DataRow("same prefix different suffix", "same prefix but different suffix")]
-    public void TrimPrefix_DifferentStrings_ReturnsTrimmedStrings(string first, string second)
+    public void TrimPrefix_DifferentStrings_ReturnsTrimmedStrings(string s1, string s2)
     {
-        var result = _stringManipulator.TrimPrefix(first, second);
+        var result = _stringManipulator.TrimPrefix(s1, s2);
 
         Assert.AreEqual("different suffix", result.Item1);
         Assert.AreEqual("but different suffix", result.Item2);
@@ -86,9 +86,9 @@ public class StringManipulatorTests
 
     [TestMethod]
     [DataRow("different prefix same suffix", "different prefix but same suffix")]
-    public void TrimSuffix_DifferentStrings_ReturnsTrimmedStrings(string first, string second)
+    public void TrimSuffix_DifferentStrings_ReturnsTrimmedStrings(string s1, string s2)
     {
-        var result = _stringManipulator.TrimSuffix(first, second);
+        var result = _stringManipulator.TrimSuffix(s1, s2);
 
         Assert.AreEqual("different prefix", result.Item1);
         Assert.AreEqual("different prefix but", result.Item2);
--- HomeWorkTests/StringsComparerTests.cs
+++ HomeWorkTests/StringsComparerTests.cs
@@ -13,20 +13,20 @@ public class StringsComparerTests
     private readonly StringsComparer _comparer;
     private readonly Mock<ILevenshteinDistance> _levenshteinCalculatorMock;
     private readonly Mock<IStringManipulator> _stringManipulatorMock;
-    private readonly Mock<ISimilarityCalculator> _similiarityCalculatorMock;
+    private readonly Mock<ISimilarityCalculator> _similarityCalculatorMock;
     private readonly Mock<IStringPreprocessor> _stringPreprocessorMock;
 
     public StringsComparerTests()
     {
         _levenshteinCalculatorMock = new Mock<ILevenshteinDistance>();
         _stringManipulatorMock = new Mock<IStringManipulator>();
-        _similiarityCalculatorMock = new Mock<ISimilarityCalculator>();
+        _similarityCalculatorMock = new Mock<ISimilarityCalculator>();
         _stringPreprocessorMock = new Mock<IStringPreprocessor>();
 
         _comparer = new StringsComparer(
             _levenshteinCalculatorMock.Object,
             _stringManipulatorMock.Object,
-            _similiarityCalculatorMock.Object,
+            _similarityCalculatorMock.Object,
             _stringPreprocessorMock.Object
         );
     }
@@ -34,22 +34,22 @@ public class StringsComparerTests
     [TestMethod]
     [DataRow("testing", "testing")]
     [DataRow("this is the same string", "this is the same string")]
-    public void Compare_IdenticalStrings_Returns100PercentSimilarity(string first, string second)
+    public void Compare_IdenticalStrings_Returns100PercentSimilarity(string s1, string s2)
     {
         _stringManipulatorMock
-            .Setup(x => x.TrimPrefix(first, second))
+            .Setup(x => x.TrimPrefix(s1, s2))
             .Returns((string.Empty, string.Empty));
         _stringManipulatorMock
-            .Setup(x => x.TrimSuffix(first, second))
+            .Setup(x => x.TrimSuffix(s1, s2))
             .Returns((string.Empty, string.Empty));
-        _levenshteinCalculatorMock.Setup(x => x.Calculate(first, second)).Returns(0);
-        _similiarityCalculatorMock
-            .Setup(x => x.CalculatePercentSimilarity(first.Length, 0))
+        _levenshteinCalculatorMock.Setup(x => x.Calculate(s1, s2)).Returns(0);
+        _similarityCalculatorMock
+            .Setup(x => x.CalculatePercentSimilarity(s1.Length, 0))
             .Returns(100);
-        _stringPreprocessorMock.Setup(x => x.Process(first)).Returns(first);
-        _stringPreprocessorMock.Setup(x => x.Process(second)).Returns(second);
+        _stringPreprocessorMock.Setup(x => x.Process(s1)).Returns(s1);
+        _stringPreprocessorMock.Setup(x => x.Process(s2)).Returns(s2);
 
-        var result = _comparer.Compare(first, second);
+        var result = _comparer.Compare(s1, s2);
 
         Assert.AreEqual(100, result);
     }
@@ -58,33 +58,33 @@ public class StringsComparerTests
     [DataRow("abcde", "vwxyz")]
     [DataRow("12345", "6789")]
     public void Compare_CompletelyDifferentStrings_Returns0PercentSimilarity(
-        string first,
-        string second
+        string s1,
+        string s2
     )
     {
-        _stringManipulatorMock.Setup(x => x.TrimPrefix(first, second)).Returns((first, second));
-        _stringManipulatorMock.Setup(x => x.TrimSuffix(first, second)).Returns((first, second));
-        _levenshteinCalculatorMock.Setup(x => x.Calculate(first, second)).Returns(5);
-        _stringPreprocessorMock.Setup(x => x.Process(first)).Returns(first);
-        _stringPreprocessorMock.Setup(x => x.Process(second)).Returns(second);
+        _stringManipulatorMock.Setup(x => x.TrimPrefix(s1, s2)).Returns((s1, s2));
+        _stringManipulatorMock.Setup(x => x.TrimSuffix(s1, s2)).Returns((s1, s2));
+        _levenshteinCalculatorMock.Setup(x => x.Calculate(s1, s2)).Returns(5);
+        _stringPreprocessorMock.Setup(x => x.Process(s1)).Returns(s1);
+        _stringPreprocessorMock.Setup(x => x.Process(s2)).Returns(s2);
 
-        var result = _comparer.Compare(first, second);
+        var result = _comparer.Compare(s1, s2);
 
         Assert.AreEqual(0, result);
     }
 
     [TestMethod]
     [DataRow("", "")]
-    public void Compare_EmptyStrings_Returns100PercentSimilarity(string first, string second)
+    public void Compare_EmptyStrings_Returns100PercentSimilarity(string s1, string s2)
     {
-        _stringManipulatorMock.Setup(x => x.TrimPrefix(first, second)).Returns((first, second));
-        _stringManipulatorMock.Setup(x => x.TrimSuffix(first, second)).Returns((first, second));
-        _levenshteinCalculatorMock.Setup(x => x.Calculate(first, "")).Returns(0);
-        _similiarityCalculatorMock.Setup(x => x.CalculatePercentSimilarity(0, 0)).Returns(100);
-        _stringPreprocessorMock.Setup(x => x.Process(first)).Returns(first);
-        _stringPreprocessorMock.Setup(x => x.Process(second)).Returns(second);
+        _stringManipulatorMock.Setup(x => x.TrimPrefix(s1, s2)).Returns((s1, s2));
+        _stringManipulatorMock.Setup(x => x.TrimSuffix(s1, s2)).Returns((s1, s2));
+        _levenshteinCalculatorMock.Setup(x => x.Calculate(s1, "")).Returns(0);
+        _similarityCalculatorMock.Setup(x => x.CalculatePercentSimilarity(0, 0)).Returns(100);
+        _stringPreprocessorMock.Setup(x => x.Process(s1)).Returns(s1);
+        _stringPreprocessorMock.Setup(x => x.Process(s2)).Returns(s2);
 
-        var result = _comparer.Compare(first, second);
+        var result = _comparer.Compare(s1, s2);
 
         Assert.AreEqual(100, result);
     }
@@ -93,18 +93,18 @@ public class StringsComparerTests
     [DataRow("", "second")]
     [DataRow("first", "")]
     public void Compare_EmptyAndNonEmptyString_Returns0PercentSimilarity(
-        string first,
-        string second
+        string s1,
+        string s2
     )
     {
-        _stringManipulatorMock.Setup(x => x.TrimPrefix(first, second)).Returns((first, second));
-        _stringManipulatorMock.Setup(x => x.TrimSuffix(first, second)).Returns((first, second));
+        _stringManipulatorMock.Setup(x => x.TrimPrefix(s1, s2)).Returns((s1, s2));
+        _stringManipulatorMock.Setup(x => x.TrimSuffix(s1, s2)).Returns((s1, s2));
         _levenshteinCalculatorMock.Setup(x => x.Calculate("", "second")).Returns(6);
         _levenshteinCalculatorMock.Setup(x => x.Calculate("first", "")).Returns(5);
-        _stringPreprocessorMock.Setup(x => x.Process(first)).Returns(first);
-        _stringPreprocessorMock.Setup(x => x.Process(second)).Returns(second);
+        _stringPreprocessorMock.Setup(x => x.Process(s1)).Returns(s1);
+        _stringPreprocessorMock.Setup(x => x.Process(s2)).Returns(s2);
 
-        var result = _comparer.Compare(first, second);
+        var result = _comparer.Compare(s1, s2);
 
         Assert.AreEqual(0, result);
     }
@@ -113,20 +113,20 @@ public class StringsComparerTests
     [DataRow("THIS IS THE SAME STRING", "this is the same string")]
     [DataRow("also This is The same String", "alsO thiS is the samE strIng")]
     public void Compare_CaseInsensitiveIdenticalStrings_ReturnsCorrectSimilarity(
-        string first,
-        string second
+        string s1,
+        string s2
     )
     {
-        _stringPreprocessorMock.Setup(x => x.Process(first)).Returns(first.ToLowerInvariant());
-        _stringPreprocessorMock.Setup(x => x.Process(second)).Returns(second.ToLowerInvariant());
+        _stringPreprocessorMock.Setup(x => x.Process(s1)).Returns(s1.ToLowerInvariant());
+        _stringPreprocessorMock.Setup(x => x.Process(s2)).Returns(s2.ToLowerInvariant());
         _stringManipulatorMock
-            .Setup(x => x.TrimPrefix(first.ToLowerInvariant(), second.ToLowerInvariant()))
+            .Setup(x => x.TrimPrefix(s1.ToLowerInvariant(), s2.ToLowerInvariant()))
             .Returns((string.Empty, string.Empty));
-        _similiarityCalculatorMock
-            .Setup(x => x.CalculatePercentSimilarity(first.Length, 0))
+        _similarityCalculatorMock
+            .Setup(x => x.CalculatePercentSimilarity(s1.Length, 0))
             .Returns(100);
 
-        var result = _comparer.Compare(first, second);
+        var result = _comparer.Compare(s1, s2);
 
         Assert.AreEqual(100, result);
     }
