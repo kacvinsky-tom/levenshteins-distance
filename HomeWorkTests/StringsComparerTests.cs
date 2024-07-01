6290eeebb815b5a938f82b111cf53f56aab4bb8e
Tomáš Kacvinský <485397@mail.muni.cz>
2024-07-01 11:43:10 +0200

    feat: add more unit tests for StringsComparer

--- HomeWorkTests/StringsComparerTests.cs
+++ HomeWorkTests/StringsComparerTests.cs
@@ -111,10 +111,13 @@ public class StringsComparerTests
         string s2
     )
     {
-        _stringPreprocessorMock.Setup(x => x.Process(s1)).Returns(s1.ToLowerInvariant());
-        _stringPreprocessorMock.Setup(x => x.Process(s2)).Returns(s2.ToLowerInvariant());
+        var s1Lower = s1.ToLowerInvariant();
+        var s2Lower = s2.ToLowerInvariant();
+
+        _stringPreprocessorMock.Setup(x => x.Process(s1)).Returns(s1Lower);
+        _stringPreprocessorMock.Setup(x => x.Process(s2)).Returns(s2Lower);
         _stringManipulatorMock
-            .Setup(x => x.TrimPrefix(s1.ToLowerInvariant(), s2.ToLowerInvariant()))
+            .Setup(x => x.TrimPrefix(s1Lower, s2Lower))
             .Returns((string.Empty, string.Empty));
         _similarityCalculatorMock
             .Setup(x => x.CalculatePercentSimilarity(s1.Length, 0))
@@ -123,5 +126,102 @@ public class StringsComparerTests
         var result = _comparer.Compare(s1, s2);
 
         Assert.AreEqual(100, result);
+    }
+
+    [TestMethod]
+    [DataRow("short string", "this is not that short string", 50)]
+    public void Compare_StringsWhereMinPercentageNotReachable_Returns0percent(
+        string s1,
+        string s2,
+        int minPercentage
+    )
+    {
+        var result = _comparer.Compare(s1, s2, 50);
+
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    [DataRow("123456", "123789", 50)]
+    [DataRow("abcdefga", "a123456a", 20)]
+    public void Compare_StringsWhereMinPercentageReachable_ReturnsCorrectPercent(
+        string s1,
+        string s2,
+        int expectedSimilarity
+    )
+    {
+        //Test1 Arrange
+        var s1LowerT1 = s1.ToLowerInvariant();
+        var s2LowerT1 = s2.ToLowerInvariant();
+
+        var trimmedPrefix1T1 = s1LowerT1.Substring(3);
+        var trimmedPrefix2T1 = s2LowerT1.Substring(3);
+        var trimmedSuffix1T1 = trimmedPrefix1T1;
+        var trimmedSuffix2T1 = trimmedPrefix2T1;
+        var levenshteinDistanceT1 = 3;
+
+        SetupMocks(
+            s1LowerT1,
+            s2LowerT1,
+            trimmedPrefix1T1,
+            trimmedPrefix2T1,
+            trimmedSuffix1T1,
+            trimmedSuffix2T1,
+            levenshteinDistanceT1,
+            expectedSimilarity
+        );
+
+        //Test2 Arrange
+        var s1LowerT2 = s1.ToLowerInvariant();
+        var s2LowerT2 = s2.ToLowerInvariant();
+
+        var trimmedPrefix1T2 = s1LowerT2.Substring(1);
+        var trimmedPrefix2T2 = s2LowerT2.Substring(1);
+        var trimmedSuffix1T2 = "bcdefg";
+        var trimmedSuffix2T2 = "123456";
+        var levenshteinDistanceT2 = 6;
+
+        SetupMocks(
+            s1LowerT2,
+            s2LowerT2,
+            trimmedPrefix1T2,
+            trimmedPrefix2T2,
+            trimmedSuffix1T2,
+            trimmedSuffix2T2,
+            levenshteinDistanceT2,
+            expectedSimilarity
+        );
+
+        var result = _comparer.Compare(s1, s2);
+
+        Assert.AreEqual(expectedSimilarity, result);
+    }
+
+    private void SetupMocks(
+        string s1Lower,
+        string s2Lower,
+        string trimmedPrefix1,
+        string trimmedPrefix2,
+        string trimmedSuffix1,
+        string trimmedSuffix2,
+        int levenshteinDistance,
+        int expectedSimilarity
+    )
+    {
+        _stringPreprocessorMock
+            .Setup(x => x.Process(It.IsAny<string>()))
+            .Returns<string>(s => s.ToLowerInvariant());
+        _stringManipulatorMock
+            .Setup(x => x.TrimPrefix(s1Lower, s2Lower))
+            .Returns((trimmedPrefix1, trimmedPrefix2));
+        _stringManipulatorMock
+            .Setup(x => x.TrimSuffix(trimmedPrefix1, trimmedPrefix2))
+            .Returns((trimmedSuffix1, trimmedSuffix2));
+        _levenshteinCalculatorMock
+            .Setup(x => x.Calculate(trimmedSuffix1, trimmedSuffix2))
+            .Returns(levenshteinDistance);
+        _similarityCalculatorMock
+            .Setup(x => x.CalculatePercentSimilarity(s1Lower.Length, levenshteinDistance))
+            .Returns(expectedSimilarity);
     }
 }
